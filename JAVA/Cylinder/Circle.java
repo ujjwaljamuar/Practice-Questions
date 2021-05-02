11af77c35b565664906d5c3f9d486454b52d4685
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2021-05-02 13:48:45 +0530

    Add files via upload

---
+++ JAVA/Cylinder/Circle.java
@@ -0,0 +1,20 @@
+package Practice_Questions.Cylinder;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = 0;
+        if (radius >= 0) {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.pow(this.radius, 2) * Math.PI;
+    }
+}
